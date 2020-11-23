2515db820be6f5adf20e33804af39d3bb486aa83
Rehan Adi Satrya <47605698+rehanadi30@users.noreply.github.com>
2020-11-23 22:59:34 +0700

    misahin file

---
+++ Minesweeper/GUI.py
@@ -0,0 +1,6 @@
+class Tile:
+    def __init__(self):
+        self.bomb = False
+
+def setup():
+    size(800,600)
--- Minesweeper/main.py
+++ Minesweeper/main.py
@@ -2,19 +2,10 @@
 import pygame
 import random as acak
 import numpy as np
+from constant import *
 
 FPS = 60
-WIDTH, HEIGHT = 600,600
-BOARD_ROWS= 10
-BOARD_COLS = 10
-#SQUARE_SIZE = WIDTH//ROWS
-LINE_WIDTH = 3
 
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
