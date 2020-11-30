38c6d4ca14b53ff68ef153fa91d945dd54cd3315
Jundullah <13518027@std.stei.itb.ac.id>
2020-11-30 14:16:11 +0700

    add fungsi addKotak, bukaKotak, printAllFact, getKotakTerbuka

--- clipsIO.py
+++ clipsIO.py
@@ -1,17 +1,51 @@
-from clips import Environment, Symbol
+from clips import Environment
+
+def addKotak(x,y,val):
+    env.build('(defrule buka-kotak-'+str(10*x+y)
+    + ' ?lama <- (kotak-tertutup (location-x '+str(x)+') (location-y '+str(y)+'))'
+    + ' ?akanbuka <- (akan-buka-kotak (location-x '+str(x)+') (location-y '+str(y)+'))'
+    + '   =>'
+    + ' (assert (kotak-terbuka (location-x '+str(x)+') (location-y '+str(y)+') (contain '+str(val)+')))'
+    + ' (retract ?lama)'
+    + ' (retract ?akanbuka)'
+    + ' )')
+    print(val)
+    new_fact = env.find_template('kotak-tertutup').new_fact()
+    new_fact['location-x'] = x
+    new_fact['location-y'] = y
+    new_fact.assertit()
+
+def bukaKotak(x,y):
+    new_fact = env.find_template('akan-buka-kotak').new_fact()
+    new_fact['location-x'] = x
+    new_fact['location-y'] = y
+    new_fact.assertit()
+
+def printAllFact():
+    for i in env.facts():
+        print(i)
+
+def getKotakTerbuka(size):
+    papan = [[0 for row in range(size)] for column in range(size)]
+    for i in env.facts():
+        if(i.template.name=='kotak-terbuka'):
+            papan[i['location-x']][i['location-y']] = 1
+    return papan
+
 
 env = Environment()
-
 env.load('miner.clp')
 
-env.assert_string('(kotak)')
-env.assert_string('(akan-buka-kotak 0 0 6)')
+addKotak(0,0,1)
+bukaKotak(0,0)
+addKotak(1,0,3)
+bukaKotak(1,0)
 
-for i in env.facts():
-    print(i)
+def start():
+    getAllFact()
+    env.run()
+    getAllFact()
+    print(getKotakTerbuka(2))
 
-env.run()
 
-print()
-for i in env.facts():
-    print(i)
+start()
