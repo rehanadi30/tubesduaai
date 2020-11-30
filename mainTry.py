20d52d2ebea825effdcf576c90a86059e7151d26
Rehan Adi Satrya <47605698+rehanadi30@users.noreply.github.com>
2020-11-30 23:45:46 +0700

    bikin maintry

---
+++ mainTry.py
@@ -0,0 +1,209 @@
+#Dependency
+import pygame
+import random as acak
+import sys
+import clipsIO
+
+sys.setrecursionlimit(1500)
+
+def papanMinesweeper(size, bom, listBomX, listBomY):
+    papan = [[0 for row in range(size)] for column in range(size)]
+
+    for num in range(bom):
+        x = listBomX[num]
+        y = listBomY[num]
+        papan[y][x] = 'X'
+        
+        """
+        x = acak.randint(0, size-1)
+        y = acak.randint(0, size-1)
+        while (papan[y][x] == 'X'):
+        #Harus ada handle ketika sekelilingnya udah bernilai 4
+        #or (papan[y][x+1]==4) or (papan[y][x-1]==4) or (papan[y-1][x-1]==4) or (papan[y-1][x+1]==4) or (papan[y-1][x]==4) or (papan[y+1][x+1]==4) or (papan[y+1][x-1]==4) or (papan[y+1][x]==4)):
+            x = acak.randint(0, size-1)
+            y = acak.randint(0, size-1)
+        papan[y][x] = 'X'
+        """
+    
+        if (x >=0 and x <= size-2) and (y >= 0 and y <= size-1):
+            if papan[y][x+1] != 'X':
+                papan[y][x+1] += 1 # center right
+        if (x >=1 and x <= size-1) and (y >= 0 and y <= size-1):
+            if papan[y][x-1] != 'X':
+                papan[y][x-1] += 1 # center left
+        if (x >= 1 and x <= size-1) and (y >= 1 and y <= size-1):
+            if papan[y-1][x-1] != 'X':
+                papan[y-1][x-1] += 1 # top left
+ 
+        if (x >= 0 and x <= size-2) and (y >= 1 and y <= size-1):
+            if papan[y-1][x+1] != 'X':
+                papan[y-1][x+1] += 1 # top right
+        if (x >= 0 and x <= size-1) and (y >= 1 and y <= size-1):
+            if papan[y-1][x] != 'X':
+                papan[y-1][x] += 1 # top center
+ 
+        if (x >=0 and x <= size-2) and (y >= 0 and y <= size-2):
+            if papan[y+1][x+1] != 'X':
+                papan[y+1][x+1] += 1 # bottom right
+        if (x >= 1 and x <= size-1) and (y >= 0 and y <= size-2):
+            if papan[y+1][x-1] != 'X':
+                papan[y+1][x-1] += 1 # bottom left
+        if (x >= 0 and x <= size-1) and (y >= 0 and y <= size-2):
+            if papan[y+1][x] != 'X':
+                papan[y+1][x] += 1 # bottom center
+    return papan
+
+def papanUser(size):
+    papanVisible = [['-' for row in range(size)] for column in range(size)]
+    return papanVisible
+
+def renderNonGui(papan):
+    for row in papan:
+        print(" ".join(str(cell) for cell in row) + "\n")
+
+def bersihkanKosong(papanUser, papanAsli, x, y, size):
+    if(papanAsli[y][x] == 0):
+        if(papanUser[y][x] == '-'):
+            papanUser[y][x] = papanAsli[y][x]
+            #print("Masuk IF")
+            if (x >=0 and x <= size-2) and (y >= 0 and y <= size-1):
+                if papanAsli[y][x+1] == 0:
+                    bersihkanKosong(papanUser, papanAsli, x+1, y, size) #tengah kanan
+                else:
+                    papanUser[y][x+1] = papanAsli[y][x+1]
+            if (x >=1 and x <= size-1) and (y >= 0 and y <= size-1):
+                if papanAsli[y][x-1] == 0:
+                    bersihkanKosong(papanUser, papanAsli, x-1, y, size) #tengah kiri
+                else:
+                    papanUser[y][x-1] = papanAsli[y][x-1]
+            if (x >= 1 and x <= size-1) and (y >= 1 and y <= size-1):
+                if papanAsli[y-1][x-1] == 0:
+                    bersihkanKosong(papanUser, papanAsli, x-1, y-1, size) #atas kiri
+                else:
+                    papanUser[y-1][x-1] = papanAsli[y-1][x-1]
+    
+            if (x >= 0 and x <= size-2) and (y >= 1 and y <= size-1):
+                if papanAsli[y-1][x+1] == 0:
+                    bersihkanKosong(papanUser, papanAsli, x+1, y-1, size) #atas kanan
+                else:
+                    papanUser[y-1][x+1] = papanAsli[y-1][x+1]
+            if (x >= 0 and x <= size-1) and (y >= 1 and y <= size-1):
+                if papanAsli[y-1][x] == 0:
+                    bersihkanKosong(papanUser, papanAsli, x, y-1, size) #atas tengah
+                else:
+                    papanUser[y-1][x] = papanAsli[y-1][x]
+    
+            if (x >=0 and x <= size-2) and (y >= 0 and y <= size-2):
+                if papanAsli[y+1][x+1] == 0:
+                    bersihkanKosong(papanUser, papanAsli, x+1, y+1, size) #bawah kanan
+                else:
+                    papanUser[y+1][x+1] = papanAsli[y+1][x+1]
+            if (x >= 1 and x <= size-1) and (y >= 0 and y <= size-2):
+                if papanAsli[y+1][x-1] == 0:
+                    bersihkanKosong(papanUser, papanAsli, x-1, y+1, size) #bawah kiri
+                else:
+                    papanUser[y+1][x-1] = papanAsli[y+1][x-1]
+            if (x >= 0 and x <= size-1) and (y >= 0 and y <= size-2):
+                if papanAsli[y+1][x] == 0:
+                    bersihkanKosong(papanUser, papanAsli, x, y+1, size) #bawah tengah
+                else:
+                    papanUser[y+1][x] = papanAsli[y+1][x]
+        papanAsli[y][x] = 0
+        papanUser[y][x] = 0
+    else:
+        print("Masuk Else")
+
+def isWon(papanAsli,size):
+    papan = clipsIO.getKotakTerbuka(size)
+    for row in range(size):
+        for cell in range(size):
+            isbom = papanAsli[row][cell]=='X'
+            if (not papan[row][cell]): #Masih ada yang belum kebuka
+                if (not isbom):
+                    return False
+            else:
+                if (isbom):
+                    return False
+    return True
+
+def gameStatus(score):
+    print("Skor kamu adalah: " + str(score))
+    isContinue = input("Coba lagi? (y/n) : ")
+    if (isContinue == 'n'):
+        return False
+    return True
+
+def main():
+    #KONSTANTA
+    arrBomX = []
+    arrBomY = []
+
+    #ALGORITMA
+    f = open("input.txt","r")
+    line = f.readline()
+    ukuranPapan = int(line)
+    print("Ukuran Papan = " + str(ukuranPapan))
+    line = f.readline()
+    jumlahBom = int(line)
+
+    for i in range(jumlahBom):
+        line = f.readline()
+        bomX, bomY = tuple(map(int,line.split(',')))
+        arrBomX.append(bomX)
+        arrBomY.append(bomY)
+
+    GameStatus = True
+    while GameStatus:
+        papanReal = papanMinesweeper(ukuranPapan, jumlahBom, arrBomX, arrBomY)
+        papanVisible = papanUser(ukuranPapan)
+        for i in range(ukuranPapan):
+            for j in range(ukuranPapan):
+                if (papanReal[j][i] == 'X'):
+                    clipsIO.addKotak(i, j, -1)
+                else:
+                    clipsIO.addKotak(i, j, int(papanReal[j][i]))
+        skor = 0
+        renderNonGui(papanReal)
+
+        while True:
+            if isWon(papanVisible, ukuranPapan) == False:
+                print("Mau buka kotak yang mana?")
+                x = int(input("X : "))
+                y = int(input("Y : "))
+
+                if (papanReal[y][x] == 'X'):
+                    print("DUARRRR!!!")
+                    renderNonGui(papanReal)
+                    GameStatus = gameStatus(skor)
+                    break
+                else:
+                    if (papanReal[y][x] == 0):
+                        bersihkanKosong(papanVisible, papanReal, x, y, ukuranPapan)
+                        renderNonGui(papanVisible)
+                        skor += 1
+                    else:
+                        papanVisible[y][x] = papanReal[y][x]
+                        renderNonGui(papanVisible)
+                        skor += 1
+            else:
+                renderNonGui(papanVisible)
+                print("Hore menang!!")
+                GameStatus = gameStatus(skor)
+                break
+                
+
+
+if __name__ == '__main__':
+    try:
+        main()
+        #clipsIO.addKotak(0,0,3)
+        #clipsIO.bukaKotak(0,0)
+        #clipsIO.start()
+    except KeyboardInterrupt:
+        print("Game Over")
+
+"""
+TO DO LISTS:
+1. GUI!!
+4. IsWon nya salah
+"""
