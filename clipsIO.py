8c137338aef102c34523b4610a9ca8cd6bc443e7
Jundullah <13518027@std.stei.itb.ac.id>
2020-11-30 14:29:07 +0700

    ganti status kotak dari int jadi bool, ngeraihin dan nambahin komentar

--- clipsIO.py
+++ clipsIO.py
@@ -1,6 +1,10 @@
 from clips import Environment
 
 def addKotak(x,y,val):
+    # dipanggil diawal (sebelum memanggil start)
+    # di loop untuk semua komponen papan
+    # val bernilai 0-8
+    # untuk bom val bernilai -1
     env.build('(defrule buka-kotak-'+str(10*x+y)
     + ' ?lama <- (kotak-tertutup (location-x '+str(x)+') (location-y '+str(y)+'))'
     + ' ?akanbuka <- (akan-buka-kotak (location-x '+str(x)+') (location-y '+str(y)+'))'
@@ -16,36 +20,40 @@ def addKotak(x,y,val):
     new_fact.assertit()
 
 def bukaKotak(x,y):
+    # dipanggil untuk membuka kotak
+    # engine hanya boleh membuka kotak yang 'chain dari pembukaan kotak yang val-nya 0'
+    # untuk melihat list kotak yang terbuka gunakan getKotakTerbuka(size)
     new_fact = env.find_template('akan-buka-kotak').new_fact()
     new_fact['location-x'] = x
     new_fact['location-y'] = y
     new_fact.assertit()
 
 def printAllFact():
+    # dipanggil internal untuk debugging clips
+    # kalau engine mau pakai silahkan aja
     for i in env.facts():
         print(i)
 
 def getKotakTerbuka(size):
-    papan = [[0 for row in range(size)] for column in range(size)]
+    # dipanggil untuk get kotak yang terbuka
+    # format keluaran berupa matriks[size][size]
+    # elemen matriks bernilai False jika kotak belum dibuka
+    # jika ada kotak yang belum dibuat (belum addKotak), nilainya juga False
+    # elemen matriks bernilai True jika kotak sudah dibuka
+    papan = [[False for row in range(size)] for column in range(size)]
     for i in env.facts():
         if(i.template.name=='kotak-terbuka'):
-            papan[i['location-x']][i['location-y']] = 1
+            papan[i['location-x']][i['location-y']] = True
     return papan
 
 
 env = Environment()
 env.load('miner.clp')
 
-addKotak(0,0,1)
-bukaKotak(0,0)
-addKotak(1,0,3)
-bukaKotak(1,0)
+# addKotak(0,0,3)
+# bukaKotak(0,0)
 
 def start():
-    getAllFact()
     env.run()
-    getAllFact()
-    print(getKotakTerbuka(2))
-
 
 start()
